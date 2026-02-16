3f41437721a59d48847bebb20fed9acb667b3877
roee738 <roee738@gmail.com>
2026-02-16 14:04:14 -0800

    add hibernate to power menu

--- .local/bin/power-menu.sh
+++ .local/bin/power-menu.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-choices="󰌾  Lock\n󰍃  Logout\n󰜉  Reboot\n󰐥  Shutdown"
+choices="󰜗  Hibernate\n󰌾  Lock\n󰍃  Logout\n󰜉  Reboot\n󰐥  Shutdown"
 
 chosen=$(echo -e "$choices" | rofi -dmenu -i -p "Power Menu")
 
 case "$chosen" in
+    "󰜗  Hibernate") systemctl hibernate ;;
     "󰌾  Lock") hyprlock ;;
     "󰍃  Logout") hyprctl dispatch exit ;;
     "󰜉  Reboot") systemctl reboot ;;
