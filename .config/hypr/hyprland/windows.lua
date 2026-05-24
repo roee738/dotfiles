332c5fd8e7abf4abea750506012532db70270220
roee738 <roee738@gmail.com>
2026-05-23 23:29:25 -0700

    Updated window rules

--- .config/hypr/hyprland/windows.lua
+++ .config/hypr/hyprland/windows.lua
@@ -18,4 +18,14 @@ hl.window_rule({
 })
 
 -- Remove border and gaps when only one tiled window is open
-hl.workspace_rule({ workspace = "w[t1]", gaps_out = 0, gaps_in = 0, border_size = 0 })
+hl.workspace_rule({ workspace = "w[1]", gaps_out = 0, gaps_in = 0, border_size = 0 })
+
+-- Floaters
+hl.window_rule({ match = { class = "qalculate-gtk" }, float = true, persistent_size = true, center = true })
+hl.window_rule({ match = { class = "nwg-look" }, float = true, persistent_size = true, center = true })
+hl.window_rule({ match = { class = "org.pulseaudio.pavucontrol" }, float = true, persistent_size = true, center = true })
+
+-- Float system dialogs
+hl.window_rule({ match = { class = "xdg-desktop-portal-gtk" }, float = true, persistent_size = true, center = true })
+-- Picture-in-picture video
+hl.window_rule({ match = { title = "Picture-in-Picture" }, float = true, pin = true, size = {400, 225}, keep_aspect_ratio = true, move = { "monitor_w-window_w-20", "monitor_h-window_h-20" } })
